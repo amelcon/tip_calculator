6aca173ff7b415867d74ce63d8e93ec5f636eabd
Alex Melcon <amelc001@fiu.edu>
2016-03-01 11:23:11 -0500

    added second file

--- tip_calculator_1.rb
+++ tip_calculator_1.rb
@@ -1,4 +1,5 @@
 puts "How much is the bill?"
 bill = gets.to_f
+
 total = '%.2f' % [bill * 0.20 + bill]
 puts "The total is $#{total}"
--- tip_calculator_2.rb
+++ tip_calculator_2.rb
@@ -3,9 +3,12 @@ bill = gets.chomp.to_f
 
 puts "What percent amount would you like to tip?"
 tip = gets.chomp.to_f / 100
+
 puts "How many people at your table?"
 split = gets.chomp.to_i
+
 total = bill * tip + bill
 puts "The total is $#{total}"
+
 final = total / split
 puts "The total per person is $#{final}"
