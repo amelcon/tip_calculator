ee84e2d3be632a6f413cc6c1e52b923c15f8d70d
Alex Melcon <amelc001@fiu.edu>
2016-03-01 11:17:08 -0500

    added secind tip file

---
+++ tip_calculator_1.rb
@@ -0,0 +1,4 @@
+puts "How much is the bill?"
+bill = gets.to_f
+total = '%.2f' % [bill * 0.20 + bill]
+puts "The total is $#{total}"
---
+++ tip_calculator_2.rb
@@ -0,0 +1,11 @@
+puts "How much is the bill?"
+bill = gets.chomp.to_f
+
+puts "What percent amount would you like to tip?"
+tip = gets.chomp.to_f / 100
+puts "How many people at your table?"
+split = gets.chomp.to_i
+total = bill * tip + bill
+puts "The total is $#{total}"
+final = total / split
+puts "The total per person is $#{final}"
